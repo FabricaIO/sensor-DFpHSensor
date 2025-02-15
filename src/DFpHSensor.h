5942194622121a777dc62cc40d48313393fedec2
Sam Groveman <samgroveman@outlook.com>
2025-02-15 15:18:55 -0500

    Update constructor

--- src/DFpHSensor.h
+++ src/DFpHSensor.h
@@ -18,7 +18,7 @@
 
 class DFpHSensor : public GenericAnalogInput {
 	public:
-		DFpHSensor(int Pin = A0, String ConfigFile = "DFpH.json");
+		DFpHSensor(int Pin = 36, String ConfigFile = "DFpH.json");
 		bool begin();
 		bool takeMeasurement();
 		
